3f11f998b9d54995827ec1ec596c8e2cc68b0d41
Nikk-05 <singh.nikhil.0528@gmail.com>
2024-12-04 11:23:22 +0530

    Recreate frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './dbConnection.js';
 
+dotenv.config();
+// connectDB();
 const app = express();
+const PORT =  process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+app.listen(PORT,()=>{
+    
+    console.log(`Server is running on port ${PORT}`)
 })
+
+// app.get('/chat-chat/api/v1/user',userRoutes)
