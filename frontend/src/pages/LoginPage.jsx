7e6b5ec3febae8735df00cb1e950344a02965255
Nikk-05 <singh.nikhil.0528@gmail.com>
2024-12-15 23:05:55 +0530

    frontend setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Intro from '../assets/introImg.png'
+
+const LoginPage = () => {
+  return (
+    <div className='w-auto h-screen flex bg-green-300'>
+        <div className='container w-full m-10 justify-center align-middle bg-white'>
+            <div className=''>
+                <h1>Login Form</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
